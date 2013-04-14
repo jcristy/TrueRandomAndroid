388b19a04d6f1fb93980dc7a800b4da9b3e92107
jcristy <jcristy@vt.edu>
2013-04-14 14:02:16 -0400

    network status is now good to go

--- src/ece5984/phase2/truerandomstudy/NetworkStatusTest.java
+++ src/ece5984/phase2/truerandomstudy/NetworkStatusTest.java
@@ -16,8 +16,8 @@ import android.content.Context;
 
 public class NetworkStatusTest extends PhoneStateListener implements Test {
 
-	int[] values = {0,0,0};
-	int[] bits = {0,0,0};
+	int[] values = {0,0,0,0,0,0};
+	int[] bits = {0,0,0,0,0,0};
 	Context context;
 	
 	ArrayList<ArrayList<DataPair>> dataPairs;
@@ -28,53 +28,40 @@ public class NetworkStatusTest extends PhoneStateListener implements Test {
 	@Override
 	public int initialize(Context context, ByteArrayOutputStream raw) {
 		this.context = context;
-		TelephonyManager tm = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
-		tm.listen(this, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS|PhoneStateListener.LISTEN_CELL_LOCATION);
-		
-		Timer time = new Timer();
 		
 		dataPairs = new ArrayList<ArrayList<DataPair>>();
         for (int i=0; i<describeTests().length;i++)
         {
         	dataPairs.add(new ArrayList<DataPair>());
         }
-		final ArrayList<ArrayList<DataPair>> fDataPairs = dataPairs;
-		final Context fcontext = context;
-		time.scheduleAtFixedRate(new TimerTask(){
-
-			@Override
-			public void run() {
-				for (int test = 0; test < describeTests().length; test++)
-				{
-					if (test <= 1)
-					{
-						DataPair toReturn = new DataPair(bits[test],values[test]);
-						bits[test] = 0;
-						values[test] = 0;
-						dataPairs.get(test).add(toReturn);
-					}
-					else
-					{
-						TelephonyManager tm = (TelephonyManager) fcontext.getSystemService(fcontext.TELEPHONY_SERVICE);
-						for( NeighboringCellInfo nc : tm.getNeighboringCellInfo())
-						{
-							Log.d("RSSI: ", ""+nc.getRssi());
-							
-						}
-						dataPairs.get(test).add(new DataPair(0,0));
-					}
-				}
-			}
-			
-		}, 0, 2000);
-		
-		return 3;
+		TelephonyManager tm = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
+		tm.listen(this, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS|PhoneStateListener.LISTEN_CELL_LOCATION);
+		return 6;
 	}
 
 	@Override
 	public ArrayList<DataPair> getData() 
 	{
-		return dataPairs.get(0);
+		int best = 0;
+		double score = Double.MAX_VALUE;
+		for(int i=0;i<describeTests().length;i++)
+		{
+			Analysis analysis = new Analysis("");
+			
+			analysis.runAnalysis(dataPairs.get(i));
+			double new_score = analysis.getScore();
+			Log.d("Network Status","Spread for "+describeTests()[i]+":"+analysis.zeros+" "+analysis.ones+" score:"+new_score);
+			if (new_score<score)
+			{
+				score = new_score;
+				best = i;
+			}
+		}
+		Log.d("Network Status","Best was "+describeTests()[best]);
+		if (score>2.5)//All Crap
+			return new ArrayList<DataPair>();
+		else
+			return dataPairs.get(best);
 	}
 
 	
@@ -89,7 +76,7 @@ public class NetworkStatusTest extends PhoneStateListener implements Test {
 	@Override
 	public String[] describeTests() {
 		
-		return new String[]{"1 bit of CDMA Strength","1 bit of EVDO Strength","1 bit of RSSI of nearest neighbor"};
+		return new String[]{"0x1 - CDMA","0x1 - EVDO","0x2 - CDMA","0x2 - EVDO","0x3 - CDMA","0x3 - EVDO"};
 	}
 
 	@Override
@@ -103,7 +90,18 @@ public class NetworkStatusTest extends PhoneStateListener implements Test {
 		bits[0] = 1;
 		values[1] = signalStrength.getEvdoDbm()&0x1;
 		bits[1] = 1;
-		Log.d("Data: ",signalStrength.getCdmaDbm()+", "+signalStrength.getEvdoDbm()+", "+signalStrength.getCdmaEcio()+", "+signalStrength.getEvdoEcio()+", "+signalStrength.getEvdoSnr());
+		values[2] = (signalStrength.getCdmaDbm()&0x2)>>1;
+		bits[2] = 1;
+		values[3] = (signalStrength.getEvdoDbm()&0x2)>>1;
+		bits[3] = 1;
+		values[4] = (signalStrength.getEvdoDbm()&0x3);
+		bits[4] = 2;
+		values[5] = (signalStrength.getEvdoDbm()&0x3);
+		bits[5] = 2;
+		for (int i=0;i<6;i++)
+			dataPairs.get(i).add(new DataPair(bits[i],values[i]));
+		//Log.d("Data: ",signalStrength.getCdmaDbm()+", "+signalStrength.getEvdoDbm()+", "+signalStrength.getCdmaEcio()+", "+signalStrength.getEvdoEcio()+", "+signalStrength.getEvdoSnr());
+		
 	}
 	
 	public void clear()
